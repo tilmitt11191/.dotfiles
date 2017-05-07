41aa54fc8a084b74765bfa9059e9fd6c15ebad19
tilmitt11191 <tilmitt11@yahoo.co.jp>
2017-05-07 20:01:11 +0900

    2017年 5月  7日 日曜日 20:01:07 JST

--- iftest.sh
+++ iftest.sh
@@ -13,3 +13,18 @@ fi
 if $isUbuntuVM; then
 	echo "host is ubuntuVM"
 fi
+
+#if [ "$(uname)" = "CYGWIN_NT-10.0" ];then
+#UNAME="$(uname)"
+#if [["$UNAME" == "CYGWIN"* ]];then
+case "$(uname)" in
+CYGWIN*)
+	echo "this is cygwin"
+esac
+
+TEST="test1234"
+echo $TEST
+if [[ $TEST == *"12"* ]]
+then
+    echo $TEST" is ok"
+fi
