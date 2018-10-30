f263285ac2e0802e17b7b7e61b9d1f708b742881
tilmitt11191 <tilmitt11@gmail.com>
2018-10-30 16:20:14 +0900

    PC 2018年 10月 30日 火曜日 16:20:04 JST

--- initial_setups/lib/keyswap.sh
+++ initial_setups/lib/keyswap.sh
@@ -3,7 +3,7 @@
 INITIALDIR=`sudo pwd`
 cd `dirname $0`
 
-echo "Swap caps for ctrl"
+echo "####Swap caps for ctrl"
 echo "XKBOPTIONS=\"ctrl:nocaps\"" | sudo tee -a /etc/default/keyboard 2>&1 >/dev/null
 
 cd $INITIALDIR
