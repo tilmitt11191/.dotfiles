bfc6a68d9312229bd578382a6d062565ee5d0325
tilmitt11191 <tilmitt11@gmail.com>
2018-10-30 11:00:10 +0900

    PC 2018年 10月 30日 火曜日 11:00:06 JST

--- initial_setups/lib/install_prezto.sh
+++ initial_setups/lib/install_prezto.sh
@@ -7,10 +7,6 @@ cd `dirname $0`
 
 dpkg -l $package | grep -E "^i.+[ \t]+$package" > /dev/null || sudo apt -y install git
 
-[ -d $HOME/.fonts/other_fonts/Powerline_fonts_for_prezto ]\
-	&& echo "exist"
-
-exit 0
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
@@ -20,6 +16,9 @@ done
 cd "${ZDOTDIR:-$HOME}"/.zprezto
 git pull
 git submodule update --init --recursive
+
+FONTS_DIR=$HOME/.fonts/other_fonts/Powerline_fonts_for_prezto
+[ -d $FONTS_DIR ] && cd $FONTS_DIR && echo "####install fonts" && bash install.sh
 
 
 cd $INITIALDIR
